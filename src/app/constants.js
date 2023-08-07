13f0117b520157d97275538fa29a6a216681783b
Fausto Gabini <fausgabini@gmail.com>
2023-08-07 14:59:38 -0300

    first commit

---
+++ src/app/constants.js
@@ -0,0 +1,22 @@
+export const INFORMATION = {
+  avatar:
+    "https://res.cloudinary.com/dgq6qxh4k/image/upload/v1689696148/Lima_oedmeh.png",
+  banner:
+    "https://images.unsplash.com/photo-1567568886202-61a5ba76061b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  title: "Lima Boutique",
+  description: "",
+  phone: "5493404438300",
+  sheet:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFSTGq5k-XYrrAwbVPMD0mpmhgnF2jYokO47AnjJj89AAKswBM02npFFdvKSD09aHa3GQD97QExwyd/pub?output=csv",
+  color: "teal",
+  social: [
+    {
+      url: "https://www.instagram.com/_lima.boutique__/",
+      name: "instagram",
+    },
+    {
+      url: "https://wa.me/5493404438300",
+      name: "whatsapp",
+    },
+  ],
+};
